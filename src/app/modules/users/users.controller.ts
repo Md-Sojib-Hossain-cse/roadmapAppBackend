592b54a023c0056010f8200c4b438d1aaca150f8
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2025-06-21 01:15:02 +0600

    catchAsync utility added:

--- src/app/modules/users/users.controller.ts
+++ src/app/modules/users/users.controller.ts
@@ -1,49 +1,40 @@
 import httpStatus from 'http-status';
-import { NextFunction, Request, Response } from 'express';
 import { UserServices } from './users.service';
 import userValidationSchema from './users.validations';
 import sendResponse from '../../utils/sendResponse';
+import catchAsync from '../../utils/catchAsync';
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const user = req.body;
+const createUser = catchAsync(async (req, res, next) => {
+  const user = req.body;
 
-    //data validating using zod
+  //data validating using zod
 
-    const zodParsedData = userValidationSchema.parse(user);
+  const zodParsedData = userValidationSchema.parse(user);
 
-    const result = await UserServices.createUserIntoDB(zodParsedData);
+  const result = await UserServices.createUserIntoDB(zodParsedData);
 
-    //send response
-    sendResponse(res, {
-      success: true,
-      statusCode: httpStatus.OK,
-      message: 'User Created Successfully!',
-      data: result,
-    });
-  } catch (error) {
-    // error send to global error handler
-    next(error);
-  }
-};
+  //send response
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'User Created Successfully!',
+    data: result,
+  });
+});
 
-const getUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const { id } = req.params;
+const getUser = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
 
-    const result = await UserServices.getUserFromDB(id);
+  const result = await UserServices.getUserFromDB(id);
 
-    //send response
-    sendResponse(res, {
-      success: true,
-      statusCode: httpStatus.OK,
-      message: 'User Data Retrieve Successfully!',
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  //send response
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'User Data Retrieve Successfully!',
+    data: result,
+  });
+});
 
 export const UserController = {
   createUser,
